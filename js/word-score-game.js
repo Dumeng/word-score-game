c94f6de15eac84c7900c45856b59331afe82b048
Dumeng <793763955@qq.com>
2017-11-16 11:43:07 +0800

    basic task completed
    basic task completed

--- js/word-score-game.js
+++ js/word-score-game.js
@@ -1,4 +1,4 @@
-var BAG_OF_LETTERS = [
+var LETTERS = [
 		new Letter('_', 2, 0),
 		new Letter('_', 2, 0),
 		new Letter('A', 9, 1),
@@ -101,61 +101,72 @@ var BAG_OF_LETTERS = [
 		new Letter('Z', 1, 10),
 ];
 
+var BAG_OF_LETTERS = [];
 var YOUR_HAND = new Array();
 var SCORE = 0;
 function startGame() {
-	addNumbersFromBag();
-	displayHand();
-	
+	SCORE = 0;
+	$( "#score-number").html(SCORE);
+	for (var i in LETTERS) {
+		BAG_OF_LETTERS[i] = new Letter(LETTERS[i].letter,LETTERS[i].numberOfStartingTiles,LETTERS[i].pointsWhenLettersUsed);
+    }
+	$("#word-history-list").empty();
+	$("div.tile-piece").show();
+	retireHand();
 };
-
-
 
 function addNumbersFromBag(){
 	console.log("your hand has:" + YOUR_HAND.length);
 	for(i=YOUR_HAND.length; i < 7; i++){
-		YOUR_HAND[i] = getAvailableLetter();
+		if(BAG_OF_LETTERS.length==0){
+			break;
+		}else{		
+			YOUR_HAND[i] = getAvailableLetter();
+		}
 	}
-	
+	if(YOUR_HAND.length==0)
+	{
+		gameOver();
+	}
 }
-
 
 function displayHand(){
 	console.log("your hand has:" + YOUR_HAND.length);
+	$( "#letters-in-bag-number").html(BAG_OF_LETTERS.length);
 	for (i = 0; i < YOUR_HAND.length; i++) {
-
-		console.log("#letter-" + (i+1) +" set to " + YOUR_HAND[i].letter);
+		//console.log("#letter-" + (i+1) +" set to " + YOUR_HAND[i].letter);
 		$( "#letter-" + (i+1)).addClass("letter-" + YOUR_HAND[i].letter);
 		$( "#points-" + (i+1)).addClass("points-" + YOUR_HAND[i].pointsWhenLettersUsed);
-		
-		
-		
-		
 		$( "#letter-" + (i+1)).html(YOUR_HAND[i].letter);
-		
 		$( "#points-" + (i+1)).html(YOUR_HAND[i].pointsWhenLettersUsed);
 	}
-	
+
 }
-
-
 
 function getAvailableLetter(){
 	var randomIndex = Math.floor(Math.random() * BAG_OF_LETTERS.length);
 	var randomLetter = BAG_OF_LETTERS.splice(randomIndex, 1);
-	console.log(randomLetter[0]);
+	//console.log(randomLetter[0]);
 	return randomLetter[0];
 }
-
 
 function findWordToUse(){
  //TODO Your job starts here.
 	alert("Your code needs to go here");	
 }
+
 function humanFindWordToUse(){
 	
 	 var humanFoundWord = $( "#human-word-input").val();
 	 console.log("Checking human workd of:" + humanFoundWord);
+	 if(humanFoundWord.length < 1){
+		 alert("Your input is not a valid word.");
+		 return;
+	 }
+	 if(humanFoundWord.length > 7){
+		 alert("Your input contains too many letters.");
+		 return;
+	 }
 	 if(isThisAWord(humanFoundWord)){
 		 if(haveLettersForWord(humanFoundWord)){
 			 successfullyAddedWord(humanFoundWord);
@@ -168,13 +179,13 @@ function humanFindWordToUse(){
 		
 }
 
-
 function successfullyAddedWord(foundWord){
-	$( "#word-history-list").append("<li>" + foundWord + "</li>");
+	t = SCORE;
 	clearClasses();
 	takeOutUsedLetters();
 	addNumbersFromBag();
 	displayHand();
+	$( "#word-history-list").append("<li>" + foundWord.toUpperCase() + " - " + (SCORE - t) + "</li>");
 	$( "#human-word-input").val('');
 	
 }
@@ -184,7 +195,6 @@ function addToScore(letterToAddToScore){
 	console.log(letterToAddToScore.pointsWhenLettersUsed + "<-Points added for " + letterToAddToScore.letter);
 	$( "#score-number").html(SCORE);
 }
-
 
 function takeOutUsedLetters(){
 	
@@ -201,37 +211,44 @@ function takeOutUsedLetters(){
 }
 
 function haveLettersForWord(aProposedWord){
-	//You could code the _ logic could go in this function
 	var wordAsArray = aProposedWord.toUpperCase().split("");
 	for (i = 0; i < wordAsArray.length; i++) {
 		var foundLetter = false;
+		var _Position = -1;
 		console.log(wordAsArray[i] + "<-For match");
 		for(ii=0; ii<YOUR_HAND.length; ii++){
 			console.log("              " + YOUR_HAND[ii].letter + "<-Checking");
+			if(YOUR_HAND[ii].letter == '_'){
+				if(!YOUR_HAND[ii].used){
+					_Position = ii;
+				}
+			}
 			if(YOUR_HAND[ii].letter == wordAsArray [i]){
 				if(!YOUR_HAND[ii].used && !foundLetter){
 					console.log("     " + YOUR_HAND[ii].letter + "<-Found");
 					YOUR_HAND[ii].used = true;
 					foundLetter = true;
-					
+					break;
 				}
 			}
 		}
 		
-		
 		if(!foundLetter){
-			resetHand();
-			return false;
+			if(_Position != -1){
+				YOUR_HAND[_Position].used = true;
+				foundLetter = true;
+			}else{
+				resetHand();
+				return false;
+			}
 		}
 	}
 	
 	return true;
 }
 
-
 function resetHand(){
-	
-	for(ii=0; ii<YOUR_HAND.length; ii++){
+	for(i=0; i<YOUR_HAND.length; i++){
 		YOUR_HAND[i].used = false;
 	}
 }
@@ -250,11 +267,16 @@ function retireHand(){
 	addNumbersFromBag();
 	displayHand();
 }
+
 function clearClasses(){
 	for(ii=0; ii < YOUR_HAND.length; ii++){
 		$("#letter-" + (ii+1)).removeClass("letter-" + YOUR_HAND[ii].letter);
 		$("#points-" + (ii+1)).removeClass("points-" + YOUR_HAND[ii].pointsWhenLettersUsed);
 	}
+}
+
+function gameOver(){
+	alert("Game Over!\nYour Score is: "+SCORE+". \nNice try.");
 }
 
 $(document).ready(function() {
@@ -269,9 +291,11 @@ $(document).ready(function() {
 	$("#retire-hand-button").click(function() {
 		retireHand();
 	});
-	$('#human-word-input').keypress(function(event) {
-		if (event.which == 13) {
-			humanFindWordToUse();
-		}
+	$("#new-game-button").click(function() {
+		startGame();
+	});
+	$("form").submit(function() {
+		humanFindWordToUse();
+		return false;
 	});
 });
