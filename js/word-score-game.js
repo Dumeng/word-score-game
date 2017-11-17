c31beefb3ad77ad87d74b4731b673717457cc7ff
Dumeng <793763955@qq.com>
2017-11-17 12:47:23 +0800

    Add comments
    Add comments for all methods

--- js/word-score-game.js
+++ js/word-score-game.js
@@ -104,6 +104,12 @@ var LETTERS = [
 var BAG_OF_LETTERS = [];
 var YOUR_HAND = new Array();
 var SCORE = 0;
+
+/**
+ Initialize HTML and global variable for a new game.
+ *
+ @method startGame
+ */
 function startGame() {
     SCORE = 0;
     $("#score-number").html(SCORE);
@@ -115,6 +121,11 @@ function startGame() {
     retireHand();
 };
 
+/**
+ Add new letters to YOUR_HAND until it has 7 letters.
+ *
+ @method addNumbersFromBag
+ */
 function addNumbersFromBag() {
     console.log("your hand has:" + YOUR_HAND.length);
     for (i = YOUR_HAND.length; i < 7; i++) {
@@ -129,6 +140,11 @@ function addNumbersFromBag() {
     }
 }
 
+/**
+ Update ".tile-piece" elements in HTML to display letters in YOUR_HAND.
+ *
+ @method displayHand
+ */
 function displayHand() {
     $(".tile-piece").hide();
     console.log("your hand has:" + YOUR_HAND.length);
@@ -144,6 +160,13 @@ function displayHand() {
 
 }
 
+/**
+ Get a random available Letter from BAG_OF_LETTERS.
+ *
+ @method getAvailableLetter
+ *
+ @return {Letter} The random available Letter.
+ */
 function getAvailableLetter() {
     var randomIndex = Math.floor(Math.random() * BAG_OF_LETTERS.length);
     var randomLetter = BAG_OF_LETTERS.splice(randomIndex, 1);
@@ -151,6 +174,11 @@ function getAvailableLetter() {
     return randomLetter[0];
 }
 
+/**
+ Find the vaild word with the highest point from current YOUR_HAND and submit it.
+ *
+ @method findWordToUse
+ */
 function findWordToUse() {
     if (YOUR_HAND.length < 2) {
         modal = UIkit.modal.blockUI("You have no way out, dude.");
@@ -201,6 +229,15 @@ function findWordToUse() {
     successfullyAddedWord(resultWord);
 }
 
+/**
+ Get all possible arrangement of letter in YOUR_HAND. The "_" will be a wildcard for all letters form "A" to "Z".
+ *
+ @method findWordToUse
+ *
+ @param {Array<String>} [letters] All available letters.{number} [len] Words length.
+ *
+ @return {Array<String>} All possible words.
+ */
 function getArrangement(letters, len) {
     var wordsList = new Array();
     if (len == 1) {
@@ -241,6 +278,11 @@ function getArrangement(letters, len) {
     }
 }
 
+/**
+ Get the player's submit and proccess it.
+ *
+ @method humanFindWordToUse
+ */
 function humanFindWordToUse() {
 
     var humanFoundWord = $("#human-word-input").val();
@@ -276,6 +318,13 @@ function humanFindWordToUse() {
     }
 }
 
+/**
+ Proccess a successful submit.
+ *
+ @method successfullyAddedWord
+ *
+ @param {String} [foundWord] The word need to be proccessed.
+ */
 function successfullyAddedWord(foundWord) {
     t = SCORE;
     clearClasses();
@@ -287,12 +336,24 @@ function successfullyAddedWord(foundWord) {
 
 }
 
+/**
+ Add the point of the letter to SCORE.
+ *
+ @method addToScore
+ *
+ @param {Letter} [letterToAddToScore] The Letter need to be proccessed.
+ */
 function addToScore(letterToAddToScore) {
     SCORE = SCORE + letterToAddToScore.pointsWhenLettersUsed;
     console.log(letterToAddToScore.pointsWhenLettersUsed + "<-Points added for " + letterToAddToScore.letter);
     $("#score-number").html(SCORE);
 }
 
+/**
+ Remove the Letters in YOUR_HAND marked as used and add their points to SCORE.
+ *
+ @method takeOutUsedLetters
+ */
 function takeOutUsedLetters() {
 
     for (ii = 0; ii < YOUR_HAND.length; ii++) {
@@ -306,7 +367,15 @@ function takeOutUsedLetters() {
     }
 
 }
-
+/**
+ Determine if Letters in YOUR_HAND can make up the submit word.
+ *
+ @method isThisAWord
+ *
+ @param {String} [aProposedWord] The word need to be proccessed.
+ *
+ @return {boolean} If Letters in YOUR_HAND can make up the word then return True. Otherwise return False.
+ */
 function haveLettersForWord(aProposedWord) {
     var wordAsArray = aProposedWord.toUpperCase().split("");
     for (i = 0; i < wordAsArray.length; i++) {
@@ -350,6 +419,15 @@ function resetHand() {
     }
 }
 
+/**
+ Determine if the submit word is a vaild word in Word_List.
+ *
+ @method isThisAWord
+ *
+ @param {String} [aProposedWord] The word need to be proccessed.
+ *
+ @return {boolean} If submit is a vaild word then return True. Otherwise return False.
+ */
 function isThisAWord(aProposedWord) {
     if (Word_List.isInList(aProposedWord)) {
         return true;
@@ -357,6 +435,11 @@ function isThisAWord(aProposedWord) {
     return false;
 }
 
+/**
+ Discard all Letters in YOUR_HAND and add new Letters from BAG_OF_LETTERS. Then update the HTML.
+ *
+ @method retireHand
+ */
 function retireHand() {
     //Loose all the points in your hand
     clearClasses();
@@ -366,6 +449,11 @@ function retireHand() {
     $("#human-word-input").val("");
 }
 
+/**
+ Update the HTML to clean YOUR_HAND display.
+ *
+ @method clearClasses
+ */
 function clearClasses() {
     for (ii = 0; ii < YOUR_HAND.length; ii++) {
         $("#letter-" + (ii + 1)).removeClass("letter-" + YOUR_HAND[ii].letter);
@@ -373,6 +461,11 @@ function clearClasses() {
     }
 }
 
+/**
+ End the game and give the result. Then update the HTML. Ask the player weather want to play a new game.
+ *
+ @method gameOver
+ */
 function gameOver() {
     $("div.tile-piece").hide();
     UIkit.modal.confirm("<p style='font-size:x-large'>Game Over! Nice try.<br /><br />Your Score is: <strong>" + SCORE + "</strong>.<br /><br />Try again?</p>", function () {
